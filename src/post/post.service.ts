becc58ee76933ec3013a0cfbe3b7ec43e24e53a3
hackrabbit <hackrabbit@qq.com>
2024-09-13 19:04:26 +0800

    feat: 数据库操作

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,13 +1,51 @@
+import { connection } from '../app/database/mysql'
+import { PostModel } from './post.model';
+
 /**
  * 获取内容列表
  */
 
-export const getPosts = () => {
-    const data = [
-        { content: '内容1' },
-        { content: '内容2' },
-        { content: '内容3' },
-    ]
+export const getPosts = async () => {
+    const statement = `
+        select post.id,post.title,post.content,JSON_OBJECT('id', user.id,'name', user.name) as user from post 
+        LEFT JOIN user ON post.userId = user.id`;
+
+    const [data] = await connection.promise().query(statement);
 
     return data;
+}
+
+/**
+ * 创建内容
+ */
+
+export const createPost = async (post: PostModel) => {
+    const statement = `INSERT INTO post SET ?`
+
+    const [data] = await connection.promise().query(statement, post);
+
+    return data;
+}
+
+/**
+ * 更新内容
+ */
+
+export const updatePost = async (postId: number, post: PostModel) => {
+    const statement = `UPDATE post SET ? WHERE id = ?`
+
+    const [data] = await connection.promise().query(statement, [post, postId]);
+
+    return data;
+}
+
+/**
+ * 删除内容
+ */
+
+export const deletePost = async (postId: number) => {
+    const statement = `DELETE FROM post WHERE id =?`
+
+    const [data] = await connection.promise().query(statement, [postId]);
+
 }
